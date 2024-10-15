d6203da722a61f7ff737083c697b77dbd4cf22ba
GuilhermeFer29 <leciguilherme@gmail.com>
2024-10-15 19:41:43 -0300

    Lista

---
+++ Aula_1_dia_15_10.py
@@ -0,0 +1,16 @@
+lista1 = []
+
+lista2 = list()
+
+lista3 = [ 1, 2, 3, 4]
+
+lista4 = list(lista3)
+
+lista5 = [1, 2, 'Texto']
+
+print(lista1)
+print(lista2)
+print(lista3)
+print(lista4)
+print(lista5)
+print('tamanho Lista  5 = ', len(lista5))
