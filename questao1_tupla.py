6591edb3141de167f5152c5efd20fe8b543f100a
GuilhermeFer29 <leciguilherme@gmail.com>
2024-10-22 21:28:42 -0300

    tuplas

---
+++ questao1_tupla.py
@@ -0,0 +1,9 @@
+tupla = ( 10 , 20 , 30)
+
+num1, num2, num3 = tupla 
+
+print(num1)
+print(num2)
+print(num3)
+print(tupla)
+
---
+++ tuplas.py
@@ -0,0 +1,11 @@
+tupla = (1,2,3)
+
+v1, v2,v3 = tupla 
+
+a = 5 
+b = 3 
+
+a, b = b, a
+
+print(a)
+print(b)
