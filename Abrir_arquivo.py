93597e44258071af26c4350cfd6dce2c2616c201
GuilhermeFer29 <leciguilherme@gmail.com>
2024-11-05 21:37:10 -0300

    Arquivo 2

---
+++ Abrir_arquivo.py
@@ -0,0 +1,2 @@
+with open('C:\\Users\\Aluno\\Desktop\\teste.txt', 'r', encoding='utf-8') as arquivo:
+    print(arquivo.read())
---
+++ Aula_Arquivos.py
@@ -0,0 +1,4 @@
+with  open('C:\\Users\\Aluno\\Desktop\\saudacao.txt','w') as arquivo:
+    arquivo.write('Hello, Word')
+
+
